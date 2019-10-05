3f67518aff6d23340c1887058328d64f8691cf54
Fabio Papa <fabtheman@gmail.com>
2019-10-05 12:27:15 -0700

    Add cookie-session to express server

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
 const PORT = 3000;
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(cookieSession({
+  name: 'battleship',
+  secret: 'thisissupersecret'
+}));
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/game/new', (req, res) => {
-  res.send('This will eventually start a new game');
+
 });
 
 app.listen(PORT, () => {
