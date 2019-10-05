104f0188dc2ffe02b5ecbbf52e51f04511f36539
Fabio Papa <fabtheman@gmail.com>
2019-10-05 13:26:38 -0700

    Make the new game route generate a new game and redirect to its url

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
+const uuid = require('uuid/v1');
+const Game = require('./lib/game.js');
 const PORT = 3000;
 
 const app = express();
@@ -10,12 +12,30 @@ app.use(cookieSession({
   secret: 'thisissupersecret'
 }));
 
+const newPlayer = () => {
+  return uuid();
+};
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/game/new', (req, res) => {
+  let playerID;
+  if (req.session.player_id) {
+    playerID = req.session.playerID;
+  } else {
+    playerID = newPlayer();
+    req.session.playerID = playerID;
+  }
 
+  const game = new Game(playerID);
+
+  res.redirect(`/game/${game.id}`);
+});
+
+app.get('/game/:id', (req, res) => {
+  res.send("This is where the game will go!");
 });
 
 app.listen(PORT, () => {
