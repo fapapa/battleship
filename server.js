417ee339f837db60001087c977c590b4037fa7c1
Fabio Papa <fabtheman@gmail.com>
2019-10-05 12:01:51 -0700

    Set up express to listen on port 3000

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = 3000;
 
 const app = express();
 
@@ -6,4 +7,8 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.listen(PORT, () => {
+  console.log(`Serving up Battleship on port ${PORT}.`);
 });
